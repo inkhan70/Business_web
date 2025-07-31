b8c512e30b5335f8f3a2701bef4dc0f2180c24bb
inayat khan <inayatkhan091@gmail.com>
2025-07-31 15:42:55 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  projectId: "distributors-connect-vrglt",
+  appId: "1:419892415781:web:6eafb3827013130dcb1ecc",
+  storageBucket: "distributors-connect-vrglt.firebasestorage.app",
+  apiKey: "AIzaSyCPsC3RzJkiTELNQVmc-UPlz47aZI0nVNM",
+  authDomain: "distributors-connect-vrglt.firebaseapp.com",
+  messagingSenderId: "419892415781",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, db, storage };
