1bd03d3a5cbb765d5fc55b49c417a6d0c2ff0563
inayat khan <inayatkhan091@gmail.com>
2025-10-15 12:36:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "distributors-connect-vrglt",
+  "appId": "1:419892415781:web:6eafb3827013130dcb1ecc",
+  "apiKey": "AIzaSyCPsC3RzJkiTELNQVmc-UPlz47aZI0nVNM",
+  "authDomain": "distributors-connect-vrglt.firebaseapp.com",
+  "measurementId": "G-Q8MF2TZXHB",
+  "messagingSenderId": "419892415781"
+};
