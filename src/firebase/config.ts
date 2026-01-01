ba4be90795d2b6f7b71bd43cc46d6882d92417e6
inayat khan <inayatkhan091@gmail.com>
2026-01-01 00:05:06 +0000

    This is the Google services.Jason file make the app configure to this, {

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,8 +2,8 @@
 export const firebaseConfig = {
   "projectId": "distributors-connect-vrglt",
   "appId": "1:419892415781:web:6eafb3827013130dcb1ecc",
-  "storageBucket": "distributors-connect-vrglt.appspot.com",
-  "apiKey": "AIzaSyCPsC3RzJkiTELNQVmc-UPlz47aZI0nVNM",
+  "storageBucket": "distributors-connect-vrglt.firebasestorage.app",
+  "apiKey": "AIzaSyBHCuunoOdE8ZIWTrlAKBGGHlShvgjn_VI",
   "authDomain": "distributors-connect-vrglt.firebaseapp.com",
   "measurementId": "G-Q8MF2TZXHB",
   "messagingSenderId": "419892415781"
